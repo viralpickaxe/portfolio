392f5a30209084b57d78060fb9ab58b34740af94
Jamie Davies <viralpickaxe@gmail.com>
2014-06-05 12:09:17 +0100

    Name roll looks nicer on first slide

--- assets/js/code.js
+++ assets/js/code.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   var margintop;
   margintop = ($(window).height()-$('.macbook-container').height())/2;
   $('.macbook-container').css('margin-top',margintop);
+
+  $('.name').fitText(0.6);
+  $('.role').fitText(1);
 });
 
 $( window ).resize(function() {
