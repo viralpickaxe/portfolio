fa1d07aca8f402057ea14452fa5ce981826687be
Jamie Davies <viralpickaxe@gmail.com>
2014-07-27 03:38:23 +0100

    Added relations tracker info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 // @codekit-append "utilities/jquery.js"
 // @codekit-append "utilities/fittext.js"
 // @codekit-append "utilities/flowtype.js"
-// @codekit-append "utilities/typer.js"
 
 // @codekit-append "elements/base.js"
 // @codekit-append "elements/buttons.js"
